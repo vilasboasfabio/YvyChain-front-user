9c0031ad79e18a65ee227941827d734d315c9321
vilasboasfabio <vilasboasfabio564@gmail.com>
2024-05-06 20:02:12 -0300

    feat: add new report

--- src/screens/RelatorioSustentabilidade/index.jsx
+++ src/screens/RelatorioSustentabilidade/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import tw from 'tailwind-react-native-classnames';
 import { useRoute } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
 const RelatorioSustentabilidade = () => {
+    const navigation = useNavigation();
     const route = useRoute();
     const [relatorio, setRelatorio] = useState(null);
     const apiUrl = process.env.EXPO_PUBLIC_API_URL;
@@ -28,27 +30,36 @@ const RelatorioSustentabilidade = () => {
 
         return (
             <>
-                <Text style={tw`text-lg`}>Relatório de Sustentabilidade gerado para {relatorio.empresaNome}.</Text>
+                <Text style={tw`text-lg`}>Relatório de Sustentabilidade elaborado para {relatorio.empresaNome}.</Text>
                 <Text style={tw`text-lg mt-2`}>
-                    Este relatório abrange diversos aspectos da sustentabilidade ambiental da empresa, incluindo análises detalhadas sobre os setores e produtos.
+                    A nota de impacto ambiental da empresa é {relatorio.notaImpactoAmbiental}.
                 </Text>
                 <Text style={tw`text-lg mt-2`}>
-                    A nota de impacto ambiental da empresa é {relatorio.notaImpactoAmbiental}. Abaixo estão os detalhes dos setores e produtos:
-                </Text>
-                <Text style={tw`text-lg mt-2`}>
-                    - Média da nota de impacto ambiental dos setores: {relatorio.mediaNotaSetores}
+                    Média da nota de impacto ambiental dos setores: {relatorio.mediaNotaSetores}
                 </Text>
                 <Text style={tw`text-lg`}>
-                    - Média da nota de impacto ambiental dos produtos: {relatorio.mediaNotaProdutos}
+                    Média da nota de impacto ambiental dos produtos: {relatorio.mediaNotaProdutos}
                 </Text>
                 <Text style={tw`text-lg mt-2`}>
-                    O setor com maior impacto ambiental é {relatorio.setorComMaiorImpacto.nome} com uma nota de {relatorio.setorComMaiorImpacto.impacto_ambiental}.
+                    Setor com maior impacto ambiental: {relatorio.setorComMaiorImpacto.nome} (Nota: {relatorio.setorComMaiorImpacto.impacto_ambiental})
                 </Text>
                 <Text style={tw`text-lg`}>
-                    O setor com menor impacto ambiental é {relatorio.setorComMenorImpacto.nome} com uma nota de {relatorio.setorComMenorImpacto.impacto_ambiental}.
+                    Setor com menor impacto ambiental: {relatorio.setorComMenorImpacto.nome} (Nota: {relatorio.setorComMenorImpacto.impacto_ambiental})
                 </Text>
                 <Text style={tw`text-lg mt-2`}>
-                    Essas informações ajudam a identificar áreas onde a empresa pode melhorar suas práticas ambientais e reduzir o impacto no meio ambiente.
+                    Produto com maior impacto ambiental: {relatorio.produtoComMaiorImpacto.nome} (Nota: {relatorio.produtoComMaiorImpacto.nota_impacto_ambiental})
+                </Text>
+                <Text style={tw`text-lg`}>
+                    Produto com menor impacto ambiental: {relatorio.produtoComMenorImpacto.nome} (Nota: {relatorio.produtoComMenorImpacto.nota_impacto_ambiental})
+                </Text>
+                <Text style={tw`text-lg mt-2`}>
+                    Insumo com maior impacto ambiental: {relatorio.insumoComMaiorImpacto.nome} (Nota: {relatorio.insumoComMaiorImpacto.max_impacto})
+                </Text>
+                <Text style={tw`text-lg mt-2`}>
+                    Média de consumo de recursos dos produtos: {relatorio.mediaConsumoRecursos}
+                </Text>
+                <Text style={tw`text-lg mt-2`}>
+                    Este relatório visa proporcionar uma visão clara do desempenho ambiental da empresa, destacando áreas de alta e baixa eficiência em termos de impacto ambiental.
                 </Text>
             </>
         );
@@ -58,6 +69,14 @@ const RelatorioSustentabilidade = () => {
         <ScrollView style={tw`flex-1 bg-white p-4`}>
             <Text style={tw`text-xl font-bold mb-4`}>Relatório de Sustentabilidade</Text>
             {formatRelatorio(relatorio)}
+            <View>
+                
+                <TouchableOpacity style={tw`mt-5 bg-green-500 p-3 rounded-lg`} onPress={() =>  navigation.navigate('Home')}>
+                    <Text style={tw`text-white text-center font-bold`}>Voltar</Text>
+                </TouchableOpacity>
+
+            
+            </View>
         </ScrollView>
     );
 };
