249e7dec934419d813bf33e69e80d91f9222114b
vilasboasfabio <vilasboasfabio564@gmail.com>
2024-05-06 19:07:46 -0300

    fix: adjust drawer style

--- src/routes/drawer.routes.jsx
+++ src/routes/drawer.routes.jsx
@@ -9,36 +9,285 @@ import VerProdutos from "../screens/VerProdutos";
 import StackRoutes from "./stack.routes";
 import PaginaGerenciamento from "../screens/PaginaGerenciamento";
 import LoginScreen from "../screens/LoginScreen";
+import { View, Image } from "react-native";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerRoutes = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="VerEmpresas" component={VerEmpresas} />
-      <Drawer.Screen name="DetalhesEmpresa" component={DetalhesEmpresa} 
-      options={{
-        drawerItemStyle: { height: 0 },  // Esconde o item no menu
-        unmountOnBlur: true  // Desmonta a tela ao perder o foco
-      }} 
+    <Drawer.Navigator
+      screenOptions={{
+        drawerActiveTintColor: "white",
+        headerTintColor: "white",
+        drawerLabelStyle: {
+          color: 'white',
+        },
+      }}
+    >
+
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: "Home",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}
       />
-      <Drawer.Screen name="VerFornecedores" component={VerFornecedores} />
-      <Drawer.Screen name="DetalhesFornecedor" component={DetalhesFornecedor} 
-      options={{
-        drawerItemStyle: { height: 0 },  // Esconde o item no menu
-        unmountOnBlur: true  // Desmonta a tela ao perder o foco
-      }} 
+
+      <Drawer.Screen
+        name="VerEmpresas"
+        component={VerEmpresas}
+        options={{
+          title: "Empresas",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}
       />
-      <Drawer.Screen name="VerProdutos" component={VerProdutos} />
-      <Drawer.Screen name="StackRoutes" component={StackRoutes} options={{
-        drawerItemStyle: { height: 0 },  // Esconde o item no menu
-        unmountOnBlur: true  // Desmonta a tela ao perder o foco
-      }} />
-      <Drawer.Screen name="PaginaGerenciamento" component={PaginaGerenciamento} 
-      
+
+      <Drawer.Screen
+        name="DetalhesEmpresa"
+        component={DetalhesEmpresa}
+        options={{
+          drawerItemStyle: { height: 0 }, // Esconde o item no menu
+          unmountOnBlur: true, // Desmonta a tela ao perder o foco
+          title: "Empresas",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}
       />
-      <Drawer.Screen name="LoginScreen" component={LoginScreen} />
+
+      <Drawer.Screen
+        name="VerFornecedores"
+        component={VerFornecedores}
+        options={{
+          title: "Fornecedores",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}
+      />
+
+      <Drawer.Screen
+        name="DetalhesFornecedor"
+        component={DetalhesFornecedor}
+        options={{
+          drawerItemStyle: { height: 0 }, // Esconde o item no menu
+          unmountOnBlur: true, // Desmonta a tela ao perder o foco
+          title: "Fornecedores",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}
+      />
+
+      <Drawer.Screen name="VerProdutos" component={VerProdutos} 
+        options={{
+          title: "Produtos",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}/>
+
+      <Drawer.Screen
+        name="StackRoutes"
+        component={StackRoutes}
+        options={{
+          drawerItemStyle: { height: 0 }, // Esconde o item no menu
+          unmountOnBlur: true, // Desmonta a tela ao perder o foco
+          title: "StackRoutes",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          )
+        }}
+      />
+
+      <Drawer.Screen
+        name="PaginaGerenciamento"
+        component={PaginaGerenciamento}
+        options={{
+          title: "Gerenciamento",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}
+      />
+
+      <Drawer.Screen name="LoginScreen" component={LoginScreen} 
+       options={{
+          title: "Login",
+          headerTitle: "",
+          headerTitle: () => (
+            <View style={{ flexDirection: "row", alignItems: "right" }}>
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{
+                  width: 75,
+                  height: 75,
+                  marginLeft: 260,
+                  marginBottom: 20,
+                  marginTop: 18,
+                }}
+              />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: "#3B5B30",
+          },
+          drawerStyle: {
+            backgroundColor: "#3B5B30",
+            textColor: "#fff",
+          },
+        }}/>
+
     </Drawer.Navigator>
   );
 };
