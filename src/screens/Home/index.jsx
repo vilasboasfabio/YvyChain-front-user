87d022cde8e236e112caf63b12847736c0c98a27
camisbersann <camila.bersan@aluno.senai.br>
2024-05-06 19:56:37 -0300

    feat: intallations fonts

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,11 +1,24 @@
 import { View, Text } from 'react-native';
 import styles from './styles';
 import Title from '../../components/Title';
+import { useFonts } from 'expo-font';
+import { Aboreto_400Regular } from '@expo-google-fonts/aboreto';
+import { ArimaMadurai_100Thin } from '@expo-google-fonts/arima-madurai'; // Corrigido para o nome correto, assumindo que você queria 'Arima Madurai'
+import { ReemKufiFun_400Regular } from '@expo-google-fonts/reem-kufi-fun';
 
 export default function Home() {
+  const [fontsLoaded] = useFonts({
+    Aboreto_400Regular,
+    ArimaMadurai_100Thin, // Corrigido para a importação correta
+    ReemKufiFun_400Regular,
+  });
+  if (!fontsLoaded) {
+      return <View><Text>Carregando fontes...</Text></View>;
+    }
+
   return (
     <View style={styles.container}>
-      <Title title={"Home"}/>
+      <Text style={[styles.titulo, { fontFamily: 'Aboreto_400Regular' }]}>Bem Vindos</Text>
     </View>
   )
 }
